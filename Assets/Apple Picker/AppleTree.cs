d5af2a331a98ae1a14740f617b94a5b2977f8af6
David T <David.g.thompson2020@gmail.com>
2024-09-10 15:25:23 -0400

    Initialized Movement

--- Assets/Apple Picker/AppleTree.cs
+++ Assets/Apple Picker/AppleTree.cs
@@ -30,6 +30,9 @@ public class AppleTree : MonoBehaviour {
     // Update is called once per frame
     void Update(){
         //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
         //Changing Direction
         
     }

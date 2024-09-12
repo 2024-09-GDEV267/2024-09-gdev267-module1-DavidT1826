60506cc8277e460199962f1a60b61457999ae1e9
David T <30881978+DavidT1826@users.noreply.github.com>
2024-09-12 11:36:08 -0400

    Ground

--- Assets/Apple Picker/ScoreCounter.cs
+++ Assets/Apple Picker/ScoreCounter.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; //This line enables use of uGUI classes like Text
+using TMPro;
 
 public class ScoreCounter : MonoBehaviour
 {
     [Header("Dynamic")]
     public int score = 0;
     private Text uiText;
+    public TextMeshProUGUI otherText;
 
 
     // Start is called before the first frame update
     void Start()
     {
         uiText = GetComponent<Text>(); 
+        otherText = GetComponent<TextMeshProUGUI>(); //This is what to do if you use TextMeshPro instead of Text
     }
 
     // Update is called once per frame

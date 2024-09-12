4a03d16dd731de8fe705d79eed2d9a357556965d
David T <David.g.thompson2020@gmail.com>
2024-09-11 21:24:25 -0400

    Missed Apple

--- Assets/Apple Picker/ApplePicker.cs
+++ Assets/Apple Picker/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,15 +10,18 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i= 0; i < numBaskets; i++)
         {
             GameObject tBasketGo = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
         }
         
     }
@@ -29,6 +33,21 @@ public class ApplePicker : MonoBehaviour
         foreach (GameObject tempGO in appleArray)
         {
             Destroy(tempGO);
+        }
+
+        //Destroy one of the Baskets
+        //Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count - 1;
+        //Get a reference to that Basket GameObject
+        GameObject basketGO = basketList[basketIndex];
+        //Remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGO);
+
+        //If there are no Baskets left, restart the game.
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Apple Picker");
         }
     }
 }

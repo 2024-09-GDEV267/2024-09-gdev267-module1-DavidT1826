044a231d868df2a6e0f7daf44406a47298a114ef
David T <30881978+DavidT1826@users.noreply.github.com>
2024-09-12 12:24:35 -0400

    Halo

--- Assets/Mission Demolition/Slingshot.cs
+++ Assets/Mission Demolition/Slingshot.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    public GameObject launchPoint;
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
     private void OnMouseEnter()
     {
-        print("Slingshot: OnMouseEnter");
+        //print("Slingshot: OnMouseEnter");
+        launchPoint.SetActive(true);
     }
 
     private void OnMouseExit()
     {
-        print("Slingshot: OnMouseExit");
+        // print("Slingshot: OnMouseExit");
+        launchPoint.SetActive(false);
     }
 }
